f90bc3711b25a8a0a4c4d8dde1b2a7c166706ab1
Anvar571 <abdurashidovanvar52@gmail.com>
2022-10-15 22:10:30 +0500

    add server site

--- router/Routers.js
+++ router/Routers.js
@@ -2,4 +2,8 @@ const routers = require("express").Router();
 
 routers.use("/", require("./HomeRoute"))
 
+routers.all('*', (req, res) => {
+    res.render('404');
+});
+
 module.exports = routers
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
+require('dotenv').config()
 const express = require("express");
 const routers = require("./router/Routers");
 const app = express();
 
 const PORT = process.env.PORT || 5000
-require('dotenv').config()
+app.set("view engine", "ejs")
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
 
 app.listen(PORT, () => {
-    console.log("listen serer on 3000 port");
+    console.log(`listen serer on ${PORT} port`);
 })
 
 function server() {
